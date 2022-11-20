913a860b0629e22dd51eb375d7ce6d42d5147070
SpeedCodeWithMoksh <shahmoksh50@gmail.com>
2022-11-20 13:10:02 +0530

    Joke Generator

---
+++ Dad Jokes/script.js
@@ -0,0 +1,38 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generatejoke)
+
+generatejoke()
+
+// USING .then()
+
+// function generatejoke() {
+//     const config = {
+
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
+
+//USING ASYNC/AWAIT
+async function generatejoke() {
+    const config = {
+
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+}
